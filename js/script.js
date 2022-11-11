295b856cbcc569af2ab58f50e36057afb89067c5
RominaMalimban <romina.lyn@hotmail.it>
2022-11-11 16:50:36 +0100

    inserimento array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 // Consegna:
 // Dato un array contenente una lista di cinque immagini (che create voi in base alle img che vi passo),
 // creare un carosello come nello screenshot allegato.
+
+// creo l'array con le immagini:
+let imgArray = [];
+
+imgArray[0] = new Image();
+imgArray[0].src = 'img/01.webp';
+
+imgArray[1] = new Image();
+imgArray[1].src = 'img/02.webp';
+
+imgArray[2] = new Image();
+imgArray[2].src = 'img/03.webp';
+
+imgArray[3] = new Image();
+imgArray[3].src = 'img/04.webp';
+
+imgArray[4] = new Image();
+imgArray[4].src = 'img/05.webp';
+
+console.log(imgArray);
